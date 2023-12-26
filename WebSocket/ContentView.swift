634512d301299b514a7bc6ba8e34b1ab3d32e573
i-seo725 <hjing719@gmail.com>
2023-12-26 10:17:57 +0900

    Initial Commit

---
+++ WebSocket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebSocket
+//
+//  Created by 이은서 on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebSocket/WebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketApp.swift
+//  WebSocket
+//
+//  Created by 이은서 on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
